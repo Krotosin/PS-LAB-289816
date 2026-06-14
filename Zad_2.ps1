036d246d50192cb11b1cebe9c05c24c9f344ef40
Krotosin <mbkozdroj@gmail.com>
2026-06-14 15:31:42 -0400

    Add files via upload

--- Zad_2.ps1
+++ Zad_2.ps1
@@ -1,5 +1,7 @@
 Write-Host "This script is one that uses the connection between a Kali Linux VM, configured to run an OpenSSH server,
 and a Windows PC that the VM is based on."
+Write-Host ""
+Write-Host "In the file [ logs.txt ], you will find a pre-filtered list of logs pulled from the OpenSSH server."
 
 "Below is a list of logs from the OpenSSH server, filtered to leave out logs of succesful opening and closing of valid sessions." > .\logs.txt
 "" >> .\logs.txt
